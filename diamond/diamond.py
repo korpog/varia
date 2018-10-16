67196904fe637856b59664a4bac8ca78bf47a8e3
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2018-10-17 00:47:34 +0200

    Initial commit

---
+++ diamond/diamond.py
@@ -0,0 +1,16 @@
+def diamond(height):
+    """Return a string resembling a diamond of specified height (measured in lines).
+    height must be an even integer.
+    """
+    result = ''
+    l, r = '/', '\\'
+    for i in range(1,height + 1):
+        if (i <= height/2):
+            line = (i * l) + (i * r)
+            result += line.center(height, ' ') + '\n'
+        else:
+            line = (height + 1 - i) * r + (height + 1 - i) * l
+            result += line.center(height, ' ') + '\n'
+    return result[:-1]
+
+print(diamond(4))
